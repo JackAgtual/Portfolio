2c0313c18915839fc19fe868c3159c683aa678a5
Jack Agtual <jaagtual@gmail.com>
2023-08-06 21:57:29 -0700

    Basic about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import About from './components/About'
 
 function App() {
   const [navOpen, setNavOpen] = useState(false)
 
   if (navOpen) return <Header navOpen={navOpen} setNavOpen={setNavOpen} />
   return (
-    <>
+    <body>
       <Header navOpen={navOpen} setNavOpen={setNavOpen} />
-    </>
+      <main>
+        <About />
+      </main>
+    </body>
   )
 }
 
---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+function About() {
+  return (
+    <div className="flex flex-col items-center py-10">
+      <div className="flex flex-col items-center md:items-start space-y-4 mx-4">
+        <h1 className="text-center text-5xl">
+          Hello, my name is <span className="font-bold">Jack Agtual</span>
+        </h1>
+        <h2 className="text-center text-3xl font-light">I'm a software engineer</h2>
+        <p className="text-center">
+          This is a small bit about me, my skills, and the work I've done.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
