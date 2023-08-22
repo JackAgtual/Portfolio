c0514c529236a411b268e834c1bb1dc5586420a0
Jack Agtual <jaagtual@gmail.com>
2023-08-21 21:34:24 -0700

    Using button instead of a for LinkButton

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,19 +1,17 @@
 import { cloneElement } from 'react'
 import { Link } from '../types/links'
 
-type LinkProps = Link
+type LinkProps = Link & { target?: '_blank' | '_self' | '_parent' | '_top' }
 
-function LinkButton({ name, href, icon }: LinkProps) {
+function LinkButton({ name, href, icon, target = '_blank' }: LinkProps) {
   return (
-    <a
-      key={name}
-      href={href}
-      target="_blank"
+    <button
       className="flex items-center justify-center space-x-2 border px-4 py-2 rounded-md"
+      onClick={() => window.open(href, target)}
     >
       {cloneElement(icon, { className: 'w-6 h-6' })}
       <p>{name}</p>
-    </a>
+    </button>
   )
 }
 
