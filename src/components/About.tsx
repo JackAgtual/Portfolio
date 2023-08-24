a860ee79fa0e53b8abbe4390f61f6088ca5b5c31
Jack Agtual <jaagtual@gmail.com>
2023-08-23 18:52:33 -0700

    Changing About section layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,21 @@
 function About() {
   return (
-    <div className="lg:grid lg:grid-cols-2">
-      <h1 className="text-4xl font-medium text-center mb-6 lg:mb-0">About Me</h1>
-      <div className="rounded-sm space-y-4 pl-4 lg:border-s-2">
-        <p>
+    <div>
+      <h1 className="text-4xl font-medium text-center mb-5">About Me</h1>
+      <div className="space-y-5 max-w-screen-md mx-auto">
+        <p className="text-center sm:text-left">
           I am a frontend-focused web developer. I love building web applications that
           solve real world problems and making those applications accessible to everyone.
           I am looking to pivot my career to the web development domain and continue
           honing my web development skills.
         </p>
-        <p>
+        <p className="text-center sm:text-left">
           I currently work as a systems engineer at Northrop Grumman. I write software in
           a language similar to C# or Java to create physics-based models of aircraft
           interactions. I'm responsible for adding new features to the models, conducting
           code reviews, and briefing analysis packages to stakeholders.
         </p>
-        <p>
+        <p className="text-center sm:text-left">
           I graduated summa cum laude from San Diego State University with a bachelor's
           degree in mechanical engineering.
         </p>
