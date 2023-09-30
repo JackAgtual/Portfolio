b4ba3fc20e6808829c999a208b2e17f353ca0442
Jack Agtual <jaagtual@gmail.com>
2023-09-29 21:37:00 -0700

    Updating project links and images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,7 @@
 import { forwardRef, ForwardedRef } from 'react'
-import laptopMock from '../assets/laptopMock.png'
+import travelPlannerScreenshot from '../assets/TravelPlannerScreenshot.png'
+import netWorthTrackerScreenshot from '../assets/NetWorthTrackerScreenshot.png'
+import workingMemoji from '../assets/workingMemoji.png'
 import ProjectCard from './ProjectCard'
 import { Project } from '../types/project'
 import SectionTitle from './SectionTitle'
@@ -8,30 +10,30 @@ const projects: Project[] = [
   {
     name: 'Travel Planner',
     description: 'Plan your next adventure with Travel Planner.',
-    image: laptopMock,
+    image: travelPlannerScreenshot,
     madeWith: ['React', 'TypeScript', 'Tailwind CSS', 'Express JS'],
     github: 'https://github.com/JackAgtual/Travel-Planner-Frontend',
     deployedLinks: {
       website: 'https://jackagtual.github.io/Travel-Planner-Frontend/',
-      demo: 'https://github.com/JackAgtual/Travel-Planner-Frontend',
+      demo: 'https://youtu.be/UU_B3bPpOEM',
     },
   },
   {
     name: 'Net Worth Tracker',
     description: 'Track your net worth over time to see how it changes.',
-    madeWith: ['React', 'TypeScript', 'Tailwind CSS', 'Firebase'],
-    image: laptopMock,
+    madeWith: ['React', 'TypeScript', 'Material UI', 'Firebase'],
+    image: netWorthTrackerScreenshot,
     github: 'https://github.com/JackAgtual/Net-Worth-Tracker',
     deployedLinks: {
       website: 'https://jackagtual.github.io/Net-Worth-Tracker/',
-      demo: 'https://github.com/JackAgtual/Net-Worth-Tracker',
+      demo: 'https://youtu.be/sASl6ts9Ad8',
     },
   },
   {
     name: 'Static solver',
     description: 'Solve static beam analysis problems.',
-    madeWith: ['React', 'TypeScript', 'Material UI'],
-    image: laptopMock,
+    madeWith: ['React', 'TypeScript', 'Material UI', 'Jest'],
+    image: workingMemoji,
     github: 'https://github.com/JackAgtual/Static-Solver',
   },
 ]
